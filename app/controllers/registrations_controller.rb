b98eb46edbe318131ee17ee92eb551d7ad97f034
CocoThePimp <jonathan.lutz@gmx.fr>
2019-03-12 12:02:17 +0100

    [PROGRESS]-Devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,18 @@
 class RegistrationsController < ApplicationController
+  
+# def new
+
+# end
+
+  
+#   private
+
+#   def sign_up_params
+#     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+#   end
+
+#   def account_update_params
+#     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+#   end
+  
 end

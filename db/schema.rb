56cfc438dcd0a3c0e1adea792c0d8e00999068b2
 NoraBourouis <bourouisnora@gmail.com>
2019-03-12 15:09:39 +0100

    [PROGRESS] Devise fixed for the is_admin, additional field and validation parts

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_103209) do
     t.string "last_name"
     t.string "first_name"
     t.text "description"
-    t.boolean "is_admin"
+    t.boolean "is_admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false

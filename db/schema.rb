9bd591f84c6f1112f5dfcbb8bfff2428a8180f8d
hnassri <nassrihamdi@gmail.com>
2019-03-13 11:56:00 +0100

    [Progress] i've create a partial for fest cart and i've done page wishlist

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_103209) do
+ActiveRecord::Schema.define(version: 2019_03_12_163346) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "fest_locations", force: :cascade do |t|
@@ -25,7 +26,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_103209) do
   end
 
   create_table "fests", force: :cascade do |t|
-    t.string "title"
+    t.citext "title"
     t.datetime "start_date"
     t.datetime "end_date"
     t.string "address"
@@ -60,7 +61,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_103209) do
     t.string "last_name"
     t.string "first_name"
     t.text "description"
-    t.boolean "is_admin", default: false
+    t.boolean "is_admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false

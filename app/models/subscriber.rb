6f953327acdc002c908636e7ae800c7df0649522
 NoraBourouis <bourouisnora@gmail.com>
2019-03-18 17:59:23 +0100

    [FIX] Delete subscriber table because widget found

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,20 @@
 class Subscriber < ApplicationRecord
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   validates :email, presence: true
+  validates_uniqueness_of :email
+
+  def self.to_csv
+    attributes = %w{id email}
+   
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |subscriber|
+        csv << subscriber.attributes.values_at(*attributes)
+    end
+  end
+end
+
+
+
 end

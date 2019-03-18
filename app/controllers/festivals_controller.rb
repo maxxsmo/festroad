93e7d3745019803a2730c9df59d00587d5f8f836
CocoThePimp <jonathan.lutz@gmx.fr>
2019-03-18 17:02:29 +0100

    [WIP] add ajax in controller

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,9 +1,17 @@
 class FestivalsController < ApplicationController
   def index 
-    @fest = Fest.all  
+    @fest = Fest.all
   end
 
   def show
     @fest = Fest.find(params[:id])
+    respond_to do |format|
+      format.html{
+        redirect_to root_path
+      }
+      format.js{
+      }
+    end
+
   end
 end

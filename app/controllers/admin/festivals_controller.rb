491f19f0ae19e4e86981d595f132444b9c5d6ce9
Marylis <stenger_marylis@hotmail.fr>
2019-03-21 23:42:45 +0100

    [progress] adding the missing fields for music type and festival type

--- app/controllers/admin/festivals_controller.rb
+++ app/controllers/admin/festivals_controller.rb
@@ -18,10 +18,14 @@ module Admin
 
     def new 
       @fest = Fest.new
+      @tag = Tag.new
+      @fest_location = FestLocation.new
     end
 
     def create 
       @fest = Fest.new(title: params[:title], description: params[:description], start_date: params[:start_date], end_date: params[:end_date], address: params[:address], website: params[:website])
+      @tag = Tag.create(music_type_id: params[:music_types], fest: @fest)
+      @fest_location = FestLocation.create(location_type_id: params[:location], fest: @fest)
       if @fest.save 
         redirect_to({action: :index}, success: "Le festival a bien été créé")
       else 

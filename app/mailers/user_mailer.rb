abfbb5aa2545b4dcd3ab58616bda7b4d3e7aa166
CocoThePimp <jonathan.lutz@gmx.fr>
2019-03-22 19:42:49 +0100

    forgetten file oops

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,16 +8,6 @@ class UserMailer < ApplicationMailer
       #on définit une variable @url qu'on utilisera dans la view d’e-mail
       @url  = 'https://festroad.herokuapp.com/utilisateur/connexion'
 
-      headers "X-SMTPAPI" => {
-        filters:{
-          templates: {
-            settings: {
-              enable: 1,
-              template_id: 'd-3374fdb66c8042659d631a192c209ed6'
-            }
-          }
-        }
-      }.to_json
       mail(to: @user.email, subject: 'Bienvenue chez nous !') 
     end
 end
